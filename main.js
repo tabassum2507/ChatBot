d2251a543875decd451ede59345c5b1e6b6816a0
tabassum2507 <10tabassum2@gmail.com>
2021-12-30 13:09:46 +0530

    chat-bot updated

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 const msgnerForm = get(".msgner-input");
 const msgnerInput = get(".text-space");
-const msgnerChat = get(".msgner-chat")
-const 
+const msgnerChat = get(".msgner-chat");
+const BOT_IMG = "bot2.png";
+const PERSON_IMG = "user.png";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Coder";
+const prompts = [
+    ["hi", "hey", "hello", "Good Morning", "Good Afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you a bot", "are you a human or a bot"],
+    ["who created you", "who made you"],
+    [
+        "your name please",
+        "your name",
+        "may i know your name",
+        "what is your name",
+        "what you call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad","bored", "tired"],
+    ["help me", "tell me a story", "tell a joke"],
+    ["ah", "yes", "ok", "okay","nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no", "not sure", "maybe", "no thanks"],
+    [" "],
+    ["haha", "ha", "lol", "hehe", "funny", "joke"]
+];
+
+const replies = [
+    ["Hello!", "Hi!", "Hey!"
+
+    ]
+]
